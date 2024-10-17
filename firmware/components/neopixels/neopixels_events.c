52da310614cd5f6a9bcadf96769a43e936efa917
Roberto Arellano <bertoarellanosegovi@gmail.com>
2024-10-17 12:39:48 -0600

    refactor: neopixels task notification support

--- firmware/components/neopixels/neopixels_events.c
+++ firmware/components/neopixels/neopixels_events.c
@@ -1,21 +1,20 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "neopixels_events.h"
 #include "esp_log.h"
 #include "esp_random.h"
 #include "esp_timer.h"
-#include "neopixels_events.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "neopixels_module.h"
 
 #define MAX_BRIGHTNESS_VALUE 25
+
+static neopixel_animation_ctx_t npx_animation_ctx;
 
 typedef struct {
   uint8_t r;
   uint8_t g;
   uint8_t b;
 } rgb_color;
-
-static TaskHandle_t neopixel_event_handler = NULL;
-static neopixel_event last_event;
 
 uint8_t get_random_uint8() {
   uint32_t entropy = esp_random();
@@ -35,60 +34,64 @@ static rgb_color get_random_color() {
 }
 
 void neopixel_scanning_event() {
-  uint8_t neopixel_idx = 0;
-  while (1) {
-    rgb_color color = get_random_color();
-    neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
-    neopixels_set_pixel(neopixel_idx, color.r, color.g, color.b);
-    neopixels_refresh();
-    neopixel_idx = ++neopixel_idx < MAX_LED_NUMBER ? neopixel_idx : 0;
-    vTaskDelay(pdMS_TO_TICKS(200));
-  }
+  static uint8_t neopixel_idx = 0;
+  rgb_color color = get_random_color();
+  neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
+  neopixels_set_pixel(neopixel_idx, color.r, color.g, color.b);
+  neopixels_refresh();
+  neopixel_idx = ++neopixel_idx < MAX_LED_NUMBER ? neopixel_idx : 0;
+  vTaskDelay(pdMS_TO_TICKS(200));
 }
 
-void neopixel_message_notify(){
-  uint8_t notify_count = 0;
-  while (notify_count < 5) {
-    neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
-    neopixels_refresh();
-    vTaskDelay(pdMS_TO_TICKS(200));
-    neopixels_set_pixels(MAX_LED_NUMBER, 50, 50, 0);
-    neopixels_refresh();
-    vTaskDelay(pdMS_TO_TICKS(200));
-    notify_count++;
-  }
-  vTaskDelete(NULL);
-  neopixel_events_run_event(neopixel_scanning_event);
-}
-
-void neopixel_llamaneitor_init(){
-  uint8_t neopixel_idx = 0;
-  while (1) {
-    neopixels_set_pixel(neopixel_idx, MAX_BRIGHTNESS_VALUE, MAX_BRIGHTNESS_VALUE, MAX_BRIGHTNESS_VALUE);
-    neopixels_refresh();
-
-    vTaskDelay(pdMS_TO_TICKS(50));
-    neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
-    neopixels_refresh();
-    vTaskDelay(pdMS_TO_TICKS(100));
-
-    neopixel_idx = ++neopixel_idx < MAX_LED_NUMBER ? neopixel_idx : 0;
-    vTaskDelay(pdMS_TO_TICKS(200));
-  }
-}
-
-void neopixel_events_stop_event() {
+void neopixel_message_notify() {
   neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
   neopixels_refresh();
-  vTaskDelete(neopixel_event_handler);
-  neopixel_event_handler = NULL;
+  vTaskDelay(pdMS_TO_TICKS(200));
+  neopixels_set_pixels(MAX_LED_NUMBER, 50, 50, 0);
+  neopixels_refresh();
+  vTaskDelay(pdMS_TO_TICKS(200));
+}
+
+void neopixel_llamaneitor_init() {
+  static uint8_t neopixel_idx = 0;
+  neopixels_set_pixel(neopixel_idx, MAX_BRIGHTNESS_VALUE, MAX_BRIGHTNESS_VALUE,
+                      MAX_BRIGHTNESS_VALUE);
+  neopixels_refresh();
+
+  vTaskDelay(pdMS_TO_TICKS(50));
+  neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
+  neopixels_refresh();
+  vTaskDelay(pdMS_TO_TICKS(100));
+
+  neopixel_idx = ++neopixel_idx < MAX_LED_NUMBER ? neopixel_idx : 0;
+  vTaskDelay(pdMS_TO_TICKS(200));
+}
+
+void neopixel_events_stop_event() { npx_animation_ctx.is_running = false; }
+
+static void npx_task() {
+  npx_animation_ctx.is_running = true;
+  while (npx_animation_ctx.is_running) {
+    npx_animation_ctx.animation();
+  }
+  neopixels_set_pixels(MAX_LED_NUMBER, 0, 0, 0);
+  neopixels_refresh();
+  vTaskDelete(NULL);
+}
+
+void neopixels_events_set_animation(neopixel_event event) {
+  npx_animation_ctx.animation = event;
+}
+
+void neopixels_events_set_notification(neopixel_event notification,
+                                       uint16_t time_ms) {
+  neopixel_event current_animation = npx_animation_ctx.animation;
+  neopixels_events_set_animation(notification);
+  vTaskDelay(pdMS_TO_TICKS(time_ms));
+  neopixels_events_set_animation(current_animation);
 }
 
 void neopixel_events_run_event(neopixel_event event) {
-  if(neopixel_event_handler != NULL) {
-    neopixel_events_stop_event();
-  }
-  last_event = event;
-  xTaskCreate(event, "effect_function", 2048, NULL, 5, &neopixel_event_handler);
-  
+  npx_animation_ctx.animation = event;
+  xTaskCreate(npx_task, "effect_function", 2048, NULL, 5, NULL);
 }
--- firmware/components/neopixels/neopixels_events.h
+++ firmware/components/neopixels/neopixels_events.h
@@ -1,8 +1,13 @@
 #pragma once
-#include <stdio.h>
 #include "esp_random.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 typedef void (*neopixel_event)(void);
+typedef struct {
+  neopixel_event animation;
+  volatile bool is_running;
+} neopixel_animation_ctx_t;
 
 void neopixel_scanning_event();
 void neopixel_events_stop_event();
